9ec50a56294988e80cf990781fbbeef78a730365
Shon Dsouza <shondsouza11@gmail.com>
2026-04-09 11:31:25 +0530

    intiial commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const tasks = [{}];
+const input = document.getElementById("input");
+const lists = document.getElementById("lists");
+const edit = document.getElementById("edit");
+const remove = document.getElementById("remove");
+
+function addEvent() {
+  const text = document.getElementById("input").value;
+  
+}
+
+let myTasks = [
+  {},
+]
