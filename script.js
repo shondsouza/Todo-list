927d7634120d8c7e4f758188d8cecaaf57127764
Shon Dsouza <shondsouza11@gmail.com>
2026-04-09 07:46:42 +0530

    completed the html

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("btn").onclick(() => {
-  const textElement = document.getElementById("text");
-  textElement.textContent = document.getElementById("texting").textContent;
-});
