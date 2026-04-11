f42d61733b433a7f2e2e6d055466a7abc3991278
Shon Dsouza <shondsouza11@gmail.com>
2026-04-11 22:44:21 +0530

    nothing

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const remove = document.getElementById("remove");
 
 function addEvent() {
   const text = document.getElementById("input").value;
-  
+  let myTasks = [{}];
+
+  console.log(text);
+  myTasks.push({ Tasks: input.value });
+  // console.log(myTasks);
+  // input.value = "";
 }
 
-let myTasks = [
-  {},
-]
+// console.log(myTasks);
