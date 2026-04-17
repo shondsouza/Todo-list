ab9d9e810f13846c6954b0a87383ba3b7902f66c
Shon Dsouza <shondsouza11@gmail.com>
2026-04-17 17:38:33 +0530

    complted the todolist

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function extractText() {
   }
   let task = { id: Date.now(), task: extractedText, isCompleted: false };
   tasks.push(task);
-  console.log(tasks);
+  // console.log(tasks);
   displayTasks();
   inputBtn.value = " ";
 }
@@ -24,10 +24,66 @@ function displayTasks() {
   listContainer.innerHTML = "";
   tasks.forEach((task) => {
     const div = document.createElement("div");
+    const span = document.createElement("span");
+    const icon = document.createElement("i");
+
+    div.classList.add("task-container");
+    icon.classList.add("remove-button");
+    span.classList.add("task-text");
+
+    if (task.isCompleted === true) {
+      const checkboxIcon = document.createElement("img");
+      checkboxIcon.src = "./images/checked.png";
+      checkboxIcon.style.width = "25px";
+      checkboxIcon.addEventListener("click", function () {
+        toggleFn(task);
+      });
+      div.appendChild(checkboxIcon);
+    } else {
+      const uncheckBoxIcon = document.createElement("img");
+      uncheckBoxIcon.src = "./images/unchecked.png";
+      uncheckBoxIcon.style.width = "25px";
+      uncheckBoxIcon.addEventListener("click", function () {
+        toggleFn(task);
+      });
+      div.appendChild(uncheckBoxIcon);
+    }
+
+    icon.classList.add("fa-solid", "fa-xmark");
+    icon.addEventListener("click", function () {
+      deleteTask(task);
+    });
+    div.appendChild(span);
+    span.textContent = task.task;
+    if (task.isCompleted === true) {
+      span.classList.add("completed");
+    }
+
+    span.addEventListener("click", function () {
+      toggleFn(task);
+    });
+    div.appendChild(icon);
     listContainer.appendChild(div);
-    const CheckboxIcon = document.createElement("img");
-    CheckboxIcon.src = "./images/checked.png";
-    div.appendChild(CheckboxIcon);
-    div.textContent = task.task;
   });
 }
+
+function toggleFn(task) {
+  task.isCompleted = !task.isCompleted;
+  displayTasks();
+}
+
+function deleteTask(task) {
+  const index = tasks.indexOf(task);
+  if (index > -1) {
+    tasks.splice(index, 1);
+  }
+
+  displayTasks();
+}
+
+inputBtn.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addToBtn.click();
+  }
+});
